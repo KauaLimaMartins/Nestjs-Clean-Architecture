56039ae9fbb693d1396a7b474869d7c88b574f7e
KauaLimaMartins <kaualimadev0601@gmail.com>
2024-01-30 21:05:34 -0300

    Implementing EnvConfig interface

--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { EnvConfig } from './env-config.interfaces';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class EnvConfigService {}
+export class EnvConfigService implements EnvConfig {
+  public constructor(private configService: ConfigService) {}
+
+  public getAppPort(): number {
+    return Number(this.configService.get<number>('PORT'));
+  }
+
+  public getNodeEnv(): string {
+    return this.configService.get<string>('NODE_ENV');
+  }
+}
