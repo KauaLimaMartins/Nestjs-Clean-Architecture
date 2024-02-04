c9603fd3675de67ab1c62136e5a2b3680bba8224
KauaLimaMartins <kaualimadev0601@gmail.com>
2024-02-03 21:54:26 -0300

    Create ClassValidatorFields and test it

---
+++ src/shared/domain/validators/class-validator-fields.ts
@@ -0,0 +1,29 @@
+import { validateSync } from 'class-validator';
+import {
+  FieldsErrors,
+  FieldsValidatorInterface,
+} from './fields-validator.interface';
+
+export abstract class ClassValidatorFields<ValidatedProps>
+  implements FieldsValidatorInterface<ValidatedProps>
+{
+  errors: FieldsErrors = null;
+  validatedData: ValidatedProps = null;
+
+  public validate(data: any): boolean {
+    const errors = validateSync(data);
+
+    if (errors.length) {
+      this.errors = {};
+
+      for (const error of errors) {
+        const field = error.property;
+        this.errors[field] = Object.values(error.constraints);
+      }
+    } else {
+      this.validatedData = data;
+    }
+
+    return !errors.length;
+  }
+}
